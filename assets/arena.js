dec1a9ade94d4ab257ee9bb6b7c32c1168c590e8
rayanadilawri <dilar840@newschool.edu>
2025-02-18 11:25:41 -0500

    added modal - not working but trying to figure it out

--- assets/arena.js
+++ assets/arena.js
@@ -26,165 +26,158 @@ channelLink.href = `https://www.are.na/channel/${channelSlug}`
 
 // Then our big function for specific-block-type rendering:
 let renderBlock = (block) => {
+    // To start, a shared `ul` where we’ll insert all our blocks
+    let channelBlocks = document.querySelector('#channel-blocks');
+    console.log(block);
 
-// To start, a shared `ul` where we’ll insert all our blocks
-let channelBlocks = document.querySelector('#channel-blocks')
-console.log(block);
+    // Links!
+    if (block.class == 'Link') {
+        let linkItem = `
+            <li class="link-block">
+                <p><em>Link</em></p>
+                <picture>
+                    <source srcset="${block.image.thumb.url}">
+                    <source srcset="${block.image.large.url}">
+                    <img src="${block.image.thumb.url}">
+                </picture>
+                <h3>${block.title}</h3>
+                ${block.description_html}
+                <p><a href="${block.source.url}">See the original ↗</a></p>
+            </li>
+        `;
+        channelBlocks.insertAdjacentHTML('beforeend', linkItem);
+    }
 
-// Links!
-if (block.class == 'Link') {
-let linkItem =
-`
-<li class="link-block">
-<p><em>Link</em></p>
-<picture>
-<source srcset="${ block.image.thumb.url }">
-<source srcset="${ block.image.large.url }">
-<img src="${ block.image.thumb.url }">
-</picture>
-<h3>${ block.title }</h3>
-${ block.description_html }
-<p><a href="${ block.source.url }">See the original ↗</a></p>
-</li>
-`
-channelBlocks.insertAdjacentHTML('beforeend', linkItem);
-}
+    // Images!
+    else if (block.class == 'Image') {
+        let linkItem = `
+            <li class="image-block">
+                <button> 
+                    <figure>
+                        <img src="${block.image.thumb.url}">
+                    </figure>
+                </button>
+                <dialog hidden>
+                    <p> I am a modal overlay!</p>
+                    <button class="close"> Close</button>
+                </dialog>
+            </li>
+        `;
+        channelBlocks.insertAdjacentHTML('beforeend', linkItem);
+    }
 
-// Images!
-else if (block.class == 'Image') {
-let linkItem =
-   `<li class="image-block">
-    <button> 
-                <li class="image-block"> <img src="${block.image.thumb.url}">
-             </button>
-                  <dialog id="diaglog">
-            <p> I am a modal overlay!</p>
-            <button class="close"> Close</button>
-        </dialog>
-       
-                </li>
-            
-`
-channelBlocks.insertAdjacentHTML('beforeend', linkItem)
-}
-
-// Text!
-else if (block.class == 'Text') {
-let linkItem =
-           `
+    // Text!
+    else if (block.class == 'Text') {
+        let linkItem = `
             <li class="text-block"> ${block.content_html}
                 <p> Quote from Arena</p>
             </li>
-             </section>`
-        channelBlocks.insertAdjacentHTML('beforeend', linkItem)
-}
+        `;
+        channelBlocks.insertAdjacentHTML('beforeend', linkItem);
+    }
 
-// Uploaded (not linked) media…
+    // Uploaded (not linked) media…
     else if (block.class == 'Attachment') {
+        let attachment = block.attachment.content_type; // Save us some repetition
 
-let attachment = block.attachment.content_type // Save us some repetition
+        // Uploaded videos!
+        if (attachment.includes('video')) {
+            let videoItem = `
+                <li class="video-block">
+                    <video controls src="${block.attachment.url}"></video>
+                </li>
+            `;
+            channelBlocks.insertAdjacentHTML('beforeend', videoItem);
+        }
 
-// Uploaded videos!
-if (attachment.includes('video')) {
-  let videoItem = `
-    <li class="video-block">
-      <video controls src="${block.attachment.url}"></video>
-    </li>
-  `;
-  channelBlocks.insertAdjacentHTML('beforeend', videoItem);
-}
+        // Uploaded PDFs!
+        // else if (attachment.includes('pdf')) {
 
+        // }
 
-// Uploaded PDFs!
-// else if (attachment.includes('pdf')) {
+        else if (attachment.includes('audio')) {
+            // …still up to you, but here’s an `audio` element:
+            let audioItem = `
+                <li class="audio-block">
+                    <audio controls src="${block.attachment.url}"></audio>
+                </li>
+            `;
+            channelBlocks.insertAdjacentHTML('beforeend', audioItem);
+            // More on audio: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
+        }
+    }
 
-// }
+    // Linked media…
+    else if (block.class == 'Media') {
+        let embed = block.embed.type;
 
-else if (attachment.includes('audio')) {
-// …still up to you, but here’s an `audio` element:
-let audioItem =
-`
-  <li class="audio-block">
-    <audio controls src="${ block.attachment.url }"></audio>
-  </li>
-`
-channelBlocks.insertAdjacentHTML('beforeend', audioItem)
-// More on audio: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/audio
-}
+        // Linked video!
+        if (embed.includes('video')) {
+            // …still up to you, but here’s an example `iframe` element:
+            let linkedVideoItem = `
+                <li class="link-block">
+                    <p><em>Linked Video</em></p>
+                    ${block.embed.html}
+                </li>
+            `;
+            channelBlocks.insertAdjacentHTML('beforeend', linkedVideoItem);
+            // More on iframe: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
+        }
 
-}
-
-// Linked media…
-else if (block.class == 'Media') {
-let embed = block.embed.type
-
-// Linked video!
-if (embed.includes('video')) {
-// …still up to you, but here’s an example `iframe` element:
-let linkedVideoItem =
-`
-<li>
-<li class="link-block">
-<p><em>Linked Video</em></p>
-${ block.embed.html }
-</li>
-`
-channelBlocks.insertAdjacentHTML('beforeend', linkedVideoItem)
-// More on iframe: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe
-}
-
-// Linked audio!
-else if (embed.includes('rich')) {
-// …up to you!
-}
-}
-
-}
+        // Linked audio!
+        else if (embed.includes('rich')) {
+            // …up to you!
+        }
+    }
+};
 
 // It‘s always good to credit your work:
 let renderUser = (user, container) => { // You can have multiple arguments for a function!
-let userAddress =
-`
-<address>
-<img src="${ user.avatar_image.display }">
-<h3>${ user.first_name }</h3>
-<p><a href="https://are.na/${ user.slug }">Are.na profile ↗</a></p>
-</address>
-`
-container.insertAdjacentHTML('beforeend', userAddress)
-}
+    let userAddress = `
+        <address>
+            <img src="${user.avatar_image.display}">
+            <h3>${user.first_name}</h3>
+            <p><a href="https://are.na/${user.slug}">Are.na profile ↗</a></p>
+        </address>
+    `;
+    container.insertAdjacentHTML('beforeend', userAddress);
+};
 
-// let initInteraction = () => {
- // let imageBlocks = document.querySelectorAll('.image-block') 
- // imageBlocks.forEach((block) => {
-  //  let openButton = block.querySelector('button')
-  //  let dialog = block.querySelector('dialog')
+let initInteraction = () => {
+    let imageBlocks = document.querySelectorAll('.image-block');
+    imageBlocks.forEach((block) => {
+        let openButton = block.querySelector('button');
+        let dialog = block.querySelector('dialog');
+        let closeButton = dialog.querySelector('button');
 
-  //  openButton.onclick = () => {
-  //    dialog.showModal()
-  //  }
-//  }
-// }
+        openButton.onclick = () => {
+            dialog.show();
+        };
 
+        closeButton.onclick = () => {
+            dialog.close();
+        };
+    });
+};
 
 // Now that we have said what we can do, go get the data:
 fetch(`https://api.are.na/v2/channels/${channelSlug}?per=100`, { cache: 'no-store' })
-.then((response) => response.json()) // Return it as JSON data
-.then((data) => { // Do stuff with the data
-console.log(data) // Always good to check your response!
-placeChannelInfo(data) // Pass the data to the first function
+    .then((response) => response.json()) // Return it as JSON data
+    .then((data) => { // Do stuff with the data
+        console.log(data); // Always good to check your response!
+        placeChannelInfo(data); // Pass the data to the first function
 
-// Loop through the `contents` array (list), backwards. Are.na returns them in reverse!
-data.contents.reverse().forEach((block) => {
-// console.log(block) // The data for a single block
-renderBlock(block) // Pass the single block data to the render function
-})
+        // Loop through the `contents` array (list), backwards. Are.na returns them in reverse!
+        data.contents.reverse().forEach((block) => {
+            // console.log(block) // The data for a single block
+            renderBlock(block); // Pass the single block data to the render function
+        });
 
-initInteraction()
+        initInteraction();
 
-// Also display the owner and collaborators:
-let channelUsers = document.querySelector('#channel-users') // Show them together
-data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers))
-renderUser(data.user, channelUsers)
-})
+        // Also display the owner and collaborators:
+        let channelUsers = document.querySelector('#channel-users'); // Show them together
+        data.collaborators.forEach((collaborator) => renderUser(collaborator, channelUsers));
+        renderUser(data.user, channelUsers);
+    });
 
